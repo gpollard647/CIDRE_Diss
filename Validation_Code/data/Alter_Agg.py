197b1e9c97a5b0e7d5276f5f3b8059b008217ae3
Gregory Pollard <68013012+gpollard647@users.noreply.github.com>
2022-07-08 15:55:00 +0100

    Delete Validation_Code/data directory

--- Validation_Code/data/Alter_Agg.py
+++
@@ -1,8 +0,0 @@
-import pandas
-
-if __name__ == "__main__":
-    
-    agg_df = pandas.read_csv("aggregated-community.csv", sep='\t')
-    agg_df = agg_df.filter(['community_id','journal_id',''], axis=1)
-    print(agg_df)
-    agg_df.to_csv('aggregated-community.csv', index=False)
